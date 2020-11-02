38ca4340dc743ed1bf4a89457dc591ac1f8d69d8
Pranav Teegavarapu <pranavnt@outlook.com>
2020-11-01 22:58:29 -0800

    basic structure working ig

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("must provide command")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "score":
+		fmt.Println(os.Args[1])
+	default:
+		fmt.Println("command not recognized")
+		os.Exit(1)
+	}
+}
