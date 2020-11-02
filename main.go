1caa0feec2ecfe7d29d1e3d45045de083bfed17b
Pranav Teegavarapu <pranavnt@outlook.com>
2020-11-01 23:00:08 -0800

    abt to find api

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("must provide command")
+		fmt.Println("You have to provide a command :)")
 		os.Exit(1)
 	}
 
 	switch os.Args[1] {
 	case "score":
-		fmt.Println(os.Args[1])
+		fmt.Println("The seahawks score is...")
 	default:
 		fmt.Println("command not recognized")
 		os.Exit(1)
