3612c94fc57c274053f58a04de343f907c409b71
Pranav Teegavarapu <pranavnt@outlook.com>
2020-11-01 23:15:56 -0800

    working on api call

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
@@ -18,4 +20,21 @@ func main() {
 		fmt.Println("command not recognized")
 		os.Exit(1)
 	}
+}
+
+func parseScore() {
+	url := "https://therundown-therundown-v1.p.rapidapi.com/affiliates"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("x-rapidapi-host", "therundown-therundown-v1.p.rapidapi.com")
+	req.Header.Add("x-rapidapi-key", "notAPIKkey")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
 }
